a7cab06f7cc32c150051528e9dd4e06cb5aeef98
Rafiul29 <rafiulislam4472@gmail.com>
2023-09-11 23:50:05 +0600

    navabr and routes setup done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,44 @@
+import { Link } from "react-router-dom";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+// import { BiUser } from "react-icons/bi";
 
 const Navbar = () => {
-
   return (
-    <header className='h-20 border-b border-gray flex items-center bg-white/80 backdrop-blur-lg  '>
+    <header className="h-20 border-b border-gray flex items-center bg-white/80 backdrop-blur-lg  ">
       <div className="wrapper flex justify-between items-center">
-        <h2 className='text-2xl font-semibold'>Shop Tronics</h2>
-        <ul className='flex gap-10'>
-          <li>link1</li>
-          <li>link2</li>
-          <li>link3</li>
-        </ul>
+
+        {/* nav logo */}
+        <h2 className="text-2xl font-semibold">Shop Tronics</h2>
+
+       {/* nav middle */}
+        <div className="flex gap-5">
+         <Link to="/">Home</Link>
+          <Link to="/products">Product</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+
+        {/* nav right */}
+        <div className="flex gap-5 justify-center items-center">
+        
+            <Link
+              className=" text-xl flex justify-center items-center gap-1"
+              to="/cart"
+            >
+              <AiOutlineShoppingCart /> Cart
+            </Link>
+        
+        
+            <Link to="/"  
+              className=" bg-black/90 text-white  px-5 py-3 rounded-xl font text-xl  hover:bg-black/75 hover:shadow-md hover:shadow-black/40duration-700"
+            >
+              Sign In
+            </Link>
+          
+        </div>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
